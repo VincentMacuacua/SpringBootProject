e58b4e1d5a610b005e9a453c5ab04ecb0f2032fc
VicenteAbelMacuacua <20240208@isctem.ac.mz>
2026-09-16 20:03:02 +0200

    Spring boot project

---
+++ src/main/java/mac/vicente/LucasAngeloProject/LucasAngeloProjectApplication.java
@@ -0,0 +1,13 @@
+package mac.vicente.LucasAngeloProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucasAngeloProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucasAngeloProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mac/vicente/LucasAngeloProject/LucasAngeloProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mac.vicente.LucasAngeloProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucasAngeloProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
